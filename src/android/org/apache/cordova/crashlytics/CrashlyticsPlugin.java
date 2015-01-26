48ae1d7766d2464b779ded63e6c7bb3a7bb1d1a5
emc2spider <emc2spider@users.noreply.github.com>
2015-01-26 14:13:38 +0000

    Remove broken Fabric SDK import

--- src/android/org/apache/cordova/crashlytics/CrashlyticsPlugin.java
+++ src/android/org/apache/cordova/crashlytics/CrashlyticsPlugin.java
@@ -2,7 +2,6 @@ package org.apache.cordova.crashlytics;
 
 import android.content.Context;
 import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 import org.apache.cordova.*;
 import org.json.JSONException;
 
@@ -14,7 +13,6 @@ public class CrashlyticsPlugin extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         Crashlytics.start((Context) cordova.getActivity());
-        Fabric.with(this, new Crashlytics());
     }
 
     private static enum BridgedMethods {
@@ -176,3 +174,4 @@ public class CrashlyticsPlugin extends CordovaPlugin {
         return false;
     }
 }
+
